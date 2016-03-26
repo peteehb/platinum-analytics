08964bd735030e93be4ece649ec89fe6d3cbb2a9
peteehb <petercollins92@gmail.com>
2016-03-26 20:10:40 +0000

    Removed sqlite as it is not needed

--- db/pnmdb/settings.py
+++ db/pnmdb/settings.py
@@ -97,12 +97,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '',
     },
-    'sqlite': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
