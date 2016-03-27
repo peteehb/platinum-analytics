69099e622b129a4712ddba2f5a9bd9242eccc768
peteehb <petercollins92@gmail.com>
2016-03-27 03:44:11 +0100

    Fix open csv writer, add relative path to init

--- mon/SyncLocalFiles.py
+++ mon/SyncLocalFiles.py
@@ -26,7 +26,7 @@ class SyncLocalFiles(object):
                         readings_failed_to_post.append(reading)
 
                 if len(readings_failed_to_post) > 0:
-                    failed_readings_writer = CsvDataWriter(filename='SensorReadingsFailed', file_header=readings_file._fieldnames)
+                    failed_readings_writer = CsvDataWriter(rel_path='/readings/', filename='SensorReadingsFailed', file_header=readings_file._fieldnames)
                     for reading in readings_failed_to_post:
                         failed_readings_writer.write(data=reading)
                     failed_readings_writer.close()
